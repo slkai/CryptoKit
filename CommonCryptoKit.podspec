332e03121d6154b26076cc9540316e91b9090210
alandeng <alandeng@meijiabang.cn>
2018-07-23 11:27:26 +0800

    init

---
+++ CommonCryptoKit.podspec
@@ -0,0 +1,29 @@
+Pod::Spec.new do |s|
+  s.name             = "CryptoKit"
+  s.summary          = "CommonCrypto in Swift"
+  s.version          = "0.0.1"
+  s.homepage         = "https://www.github.com/"
+  s.license          = 'MIT'
+  s.author           = { "alan" => "alandeng@meijiabang.cn" }
+  s.source           = {
+    :git => "https://github.com/slkai/CryptoKit",
+    :tag => s.version.to_s
+  }
+
+  s.ios.deployment_target = '8.0'
+  s.osx.deployment_target = '10.9'
+  s.tvos.deployment_target = '9.2'
+  s.watchos.deployment_target = '3.0'
+
+  s.requires_arc = true
+  s.source_files = 'Sources/**/*'
+
+  # 需要保留的文件路径
+  s.preserve_paths = '${BUILT_PRODUCTS_DIR}/CommonCryptoModuleMap/**/*'
+
+  s.pod_target_xcconfig = {
+    'SWIFT_INCLUDE_PATHS' => '${BUILT_PRODUCTS_DIR}/CommonCryptoModuleMap'
+    'SWIFT_VERSION' => '4.0'
+  }
+
+end
---
+++ Sources/String+HMAC.swift
@@ -0,0 +1,58 @@
+//
+//  String+HMAC.swift
+//  CryptoKit
+//
+//  Created by Alan on 2018/7/23.
+//  Copyright © 2018年 Alan. All rights reserved.
+//
+
+import Foundation
+import CommonCrypto
+
+public enum CryptoAlgorithm {
+    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+    
+    var HMACAlgorithm: CCHmacAlgorithm {
+        var result: Int = 0
+        switch self {
+        case .MD5:      result = kCCHmacAlgMD5
+        case .SHA1:     result = kCCHmacAlgSHA1
+        case .SHA224:   result = kCCHmacAlgSHA224
+        case .SHA256:   result = kCCHmacAlgSHA256
+        case .SHA384:   result = kCCHmacAlgSHA384
+        case .SHA512:   result = kCCHmacAlgSHA512
+        }
+        return CCHmacAlgorithm(result)
+    }
+    
+    var digestLength: Int {
+        var result: Int32 = 0
+        switch self {
+        case .MD5:      result = CC_MD5_DIGEST_LENGTH
+        case .SHA1:     result = CC_SHA1_DIGEST_LENGTH
+        case .SHA224:   result = CC_SHA224_DIGEST_LENGTH
+        case .SHA256:   result = CC_SHA256_DIGEST_LENGTH
+        case .SHA384:   result = CC_SHA384_DIGEST_LENGTH
+        case .SHA512:   result = CC_SHA512_DIGEST_LENGTH
+        }
+        return Int(result)
+    }
+}
+
+public extension String {
+    
+    func hmac(algorithm: CryptoAlgorithm, key: String) -> String {
+        let str = self.cString(using: String.Encoding.utf8)
+        let strLen = Int(self.lengthOfBytes(using: String.Encoding.utf8))
+        let digestLen = algorithm.digestLength
+        let result = UnsafeMutablePointer<CUnsignedChar>.allocate(capacity: digestLen)
+        let keyStr = key.cString(using: String.Encoding.utf8)
+        let keyLen = Int(key.lengthOfBytes(using: String.Encoding.utf8))
+        
+        CCHmac(algorithm.HMACAlgorithm, keyStr!, keyLen, str!, strLen, result)
+        let data = Data(bytes: result, count: digestLen)
+        result.deallocate(capacity: digestLen)
+        let resultString = data.base64EncodedString()
+        return resultString
+    }
+}
